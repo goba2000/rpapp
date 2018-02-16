6510881f6f8a4655ea27bf1bfd2a811943f0018f
goba2000 <36506501+goba2000@users.noreply.github.com>
2018-02-16 11:30:11 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,6 +1,11 @@
-‘use strict’;
-importScripts(‘sw-toolbox.js’);
-toolbox.precache([“index.html”]);
-toolbox.router.get(‘/iconka/*’, toolbox.cacheFirst);
-toolbox.router.get(‘/*’, toolbox.networkFirst, {
-networkTimeoutSeconds: 5});
+'use strict';
+
+importScripts('sw-toolbox.js');
+
+toolbox.precache(["index.html"]);
+
+toolbox.router.get('/images/*', toolbox.cacheFirst);
+
+toolbox.router.get('/*', toolbox.networkFirst, {
+  networkTimeoutSeconds: 5
+});
